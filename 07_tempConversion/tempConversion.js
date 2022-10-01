8ef93771179f2e49c57102432be8231d72cdf1c7
Ondrej Vymola <ovymola+git@gmail.com>
2022-10-01 21:53:05 +0200

    07_tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel = (far - 32) * 5 / 9;
+  return Math.round(cel*10)/10; 
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far = (cel * 1.8) + 32;
+  return Math.round(far*10)/10;
 };
 
 // Do not edit below this line
