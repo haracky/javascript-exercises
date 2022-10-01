fcb594b04388e707f183027016e85972b23f4367
Ondrej Vymola <ovymola+git@gmail.com>
2022-10-01 21:25:55 +0200

    03_reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let result = '';
+    for (i=0;i<input.length;i++){
+        result += input[input.length-i-1];
+    }
+    return result;
 };
 
 // Do not edit below this line
